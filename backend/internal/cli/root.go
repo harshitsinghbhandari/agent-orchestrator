fd542627a4d04cda08c97da6872f61052e25923d
Harshit Singh Bhandari <dev@theharshitsingh.com>
2026-07-31 18:39:24 +0530

    feat(pipelines): ao pipeline CLI verbs and definition CRUD

--- backend/internal/cli/root.go
+++ backend/internal/cli/root.go
@@ -197,6 +197,7 @@ func NewRootCommand(deps Deps) *cobra.Command {
 	root.AddCommand(newDevCommand(ctx))
 	root.AddCommand(newProjectCommand(ctx))
 	root.AddCommand(newSessionCommand(ctx))
+	root.AddCommand(newPipelineCommand(ctx))
 	root.AddCommand(newOrchestratorCommand(ctx))
 	root.AddCommand(newPRCommand(ctx))
 	root.AddCommand(newReviewCommand(ctx))
